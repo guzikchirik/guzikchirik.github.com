7622513f6ad012e65b10eb2566aa6d3436f031dc
Anton Chyrko <anton_chyrko@epam.com>
2022-08-01 01:12:47 +0200

    add menu and pages

---
+++ php/menu.php
@@ -0,0 +1,75 @@
+<nav id="primary_nav_wrap">
+			<ul>
+				<li class="current-menu-item"><a href="#">Home</a></li>
+				<li><a href="pages/testAutomation.html" id='testAutomation'>Test automation</a>
+					<ul>
+						<li><a href="#">UI</a></li>
+						<li><a href="#">Mobile</a></li>
+						<li><a href="#">API</a></li>
+						<li><a href="#">Tools</a>
+							<ul>
+								<li><a href="#">Build tools</a>
+									<ul>
+										<li><a href="#">Maven</a></li>
+										<li><a href="#">Gradle</a></li>
+										<li><a href="#">Ant</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Assertion frameworks</a>
+									<ul>
+										<li><a href="#">TestNG</a></li>
+										<li><a href="#">JUnit5</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Lombok</a></li>
+								<li><a href="#">Guice</a></li>
+								<li><a href="#">Cucumber</a></li>
+								<li><a href="#">Rest-Assured</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Technologies</a>
+							<ul>
+								<li><a href="#">Java</a>
+									<ul>
+										<li><a href="#">Java8</a></li>
+										<li><a href="#">Java11</a></li>
+										<li><a href="#">Java17</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Selenium WebDriver</a></li>
+								<li><a href="#">Docker</a></li>
+								<li><a href="#">Jenkins</a></li>
+							</ul>
+						</li>
+						<li><a href="#">TestReports</a>
+							<ul>
+								<li><a href="#">ReportPortal</a></li>
+								<li><a href="#">Allure</a></li>
+								<li><a href="#">MavenReport</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="pages/tasks.html" id='tasks'>Tasks</a>
+					<ul>
+						<li><a href="pages/tasks/triangle.html">Triangle</a></li>
+						<li><a href="#">Login form</a></li>
+					</ul>
+				</li>
+				<li><a href="pages/education.html" id='education'>Education</a>
+					<ul>
+						<li class="dir"><a href="#">Automated Testing in Java</a></li>
+						<li class="dir"><a href="#">Testing Fundamentals</a>
+						<li class="dir"><a href="#">Object-oriented Programming</a>
+						<li class="dir"><a href="#">API Test Automation</a>
+					</ul>
+				</li>
+				<li><a href="pages/instructions.html" id='instructions'>Instructions</a>
+					<ul>
+						<li class="dir"><a href="#">Set up Jenkins in Docker</a></li>
+						<li class="dir"><a href="#">Set up mobile testing env</a></li>
+					</ul>
+				</li>
+				<li><a href="pages/visitka.html" id='aboutMe'>About me</a></li>
+			</ul>
+		</nav>
